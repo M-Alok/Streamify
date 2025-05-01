d32335c3e403509bf74310a4b017defd5c32cfb0
Alok <ams03022001@gmail.com>
2025-05-01 09:01:33 +0530

    Backend auth functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,98 @@
-export const signup = (req, res) => {
-    res.send('signup');
+import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be atleast 6 characters' });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        const idx = Math.floor(Math.random() * 100) + 1;
+
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+
+        const newUser = await User.create({
+            fullName: fullName,
+            email: email,
+            password: password,
+            profilePic: randomAvatar,
+        });
+
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: '7d',
+        });
+
+        res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production',
+        });
+
+        res.status(200).json({ success: true, user: newUser });
+    } catch (error) {
+        console.log('Error in signup controller: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
-export const login = (req, res) => {
-    res.send('login');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields required' });
+        }
+        
+        const user = await User.findOne({ email });
+    
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: '7d',
+        });
+
+        res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production',
+        });
+
+        res.status(200).json({ success: true, user: user });
+    } catch (error) {
+        console.log('Error in login controller: ', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 export const logout = (req, res) => {
-    res.send('logout');
+    res.clearCookie('jwt');
+    res.status(200).json({ message: 'Logout successful' });
 }
