f22ea79129361b77faf2a9a3b18e8e0933218586
Alok <ams03022001@gmail.com>
2025-05-03 01:10:57 +0530

    Send and accept friend request endpoints

--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -1,10 +1,23 @@
 import express from 'express';
-import { getMyFriends, getRecommendedUsers } from '../controllers/user.controller.js';
+import { 
+    getRecommendedUsers,
+    getMyFriends,
+    sendFriendRequest,
+    acceptFriendRequest,
+    getFriendRequests,
+    getOutgoingFriendRequests,
+} from '../controllers/user.controller.js';
 import protectRoute from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute, getRecommendedUsers);
 router.get('/friends', protectRoute, getMyFriends);
+
+router.post('/friend-request/:id', protectRoute, sendFriendRequest);
+router.put('/friend-request/:id/accept', protectRoute, acceptFriendRequest);
+
+router.get('/friend-request', protectRoute, getFriendRequests);
+router.get('/outgoing-friend-request', protectRoute, getOutgoingFriendRequests);
 
 export default router;
